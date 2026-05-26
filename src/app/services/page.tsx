9dc64d95247b06216488a9a5d67d6c7893f4a098
arunkoffeekode <115154631+arunkoffeekode@users.noreply.github.com>
2026-05-26 18:10:24 +0530

    services page

---
+++ src/app/services/page.tsx
@@ -0,0 +1,150 @@
+"use client";
+import serviceBanner from "@/img/servicesIcon/services-banner.jpg";
+import Image from "next/image";
+import Forward from "../component/Forward";
+import { useState } from "react";
+
+const services = [
+  "AEPS",
+  "M-ATM Services",
+  "BBPS",
+  "Mobile & DTH Recharge",
+  "EMI Collection",
+  "CMS",
+];
+const ServicesPage = () => {
+  const [active, setActive] = useState(0);
+  return (
+    <>
+      <div className="bg-whiteColor w-full py-6 md:py-10">
+        <div className="container lg:max-w-280 xl:max-w-305 2xl:max-w-screen-2xl relative px-4 mx-auto">
+          <div className="grid grid-cols-12 gap-x-3">
+            <div className="col-span-full xl:col-span-3">
+              <div className="w-full">
+                <h2 className="text-green font-bold capitalize text-font27 leading-9.25 md:text-[35px] md:leading-11.25 mb-2 w-full">
+                  Our Services
+                </h2>
+                <p className="text-textColor text-font16 font-normal text-start leading-6.75 w-full mb-5">
+                  We provide secure, fast, and reliable digital payment and
+                  financial solutions designed to help retailers and service
+                  partners offer convenient banking and payment services to
+                  customers.
+                </p>
+                <div className="w-full hidden xl:block">
+                  {services.map((item, index) => {
+                    const isActive = active === index;
+
+                    return (
+                      <div
+                        key={index}
+                        className="flex items-start gap-3 cursor-pointer group"
+                        onClick={() => setActive(index)}
+                      >
+                        <div className="flex flex-col items-center">
+                          <div
+                            className={`w-5.5 h-5.5 rounded-full border-2 transition-all duration-300 ${
+                              isActive
+                                ? "bg-green border-green scale-110"
+                                : "bg-lightText border-lightText"
+                            }`}
+                          />
+                          {index !== services.length - 1 && (
+                            <div className="w-0.5 h-8 bg-lightText" />
+                          )}
+                        </div>
+                        <div className="pt-[-2px]">
+                          <div
+                            className={`px-2 ${
+                              isActive ? " text-green" : " text-lightText"
+                            }`}
+                          >
+                            <h3 className="text-font17 font-medium">{item}</h3>
+                          </div>
+                        </div>
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+            </div>
+            <div className="col-span-full xl:col-span-9">
+              <div className="w-full mb-5">
+                <Image
+                  src={serviceBanner}
+                  width={0}
+                  height={0}
+                  alt="Services"
+                  className="rounded-[20px]"
+                />
+              </div>
+              <div className="w-full">
+                <h2 className="text-green font-bold capitalize text-font27 leading-9.25 md:text-font40 md:leading-12.5 mb-1 w-full">
+                  AEPS
+                </h2>
+                <p className="text-textColor text-font16 font-normal text-start leading-6.75 w-full mb-5">
+                  Manage all your bill payments in one place. Electricity,
+                  water, gas, mobile, and more paid instantly with complete
+                  security.
+                </p>
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
+                  {[
+                    {
+                      id: "1.",
+                      title: "One-Stop Platform",
+                      desc: "Pay all types of bills – electricity, gas, water, DTH, mobile recharge, broadband, FASTag, insurance, loan EMIs, etc.",
+                    },
+                    {
+                      id: "2.",
+                      title: "Anytime, Anywhere Access",
+                      desc: "Available through multiple channels banks, digital wallets, mobile apps, websites, and physical agent outlets.",
+                    },
+                    {
+                      id: "3.",
+                      title: "Secure And Reliable",
+                      desc: "Offers a safe and standardized platform with real-time payment confirmation.",
+                    },
+                    {
+                      id: "4.",
+                      title: "Interoperability",
+                      desc: "Customers can pay bills from any biller through any BBPS-enabled platform, regardless of the service provider.",
+                    },
+                    {
+                      id: "5.",
+                      title: "Instant Confirmation",
+                      desc: "Receipts are generated immediately after the transaction.",
+                    },
+                    {
+                      id: "6.",
+                      title: "Multiple Payment Options",
+                      desc: "Accepts payments via UPI, net banking, debit/credit cards, cash (through agents), and mobile wallets.",
+                    },
+                  ].map((item) => (
+                    <div
+                      key={item.id}
+                      className="bg-whiteColor border border-borderColor rounded-[20px] p-5 shadow-sm transition-all duration-300"
+                    >
+                      <h2 className="text-green text-font30 font-bold leading-none mb-4">
+                        {item.id}
+                      </h2>
+
+                      <h3 className="text-textColor text-[22px] font-semibold leading-tight mb-3">
+                        {item.title}
+                      </h3>
+
+                      <p className="text-lightText text-font16 leading-7 font-normal">
+                        {item.desc}
+                      </p>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Forward />
+    </>
+  );
+};
+
+export default ServicesPage;
